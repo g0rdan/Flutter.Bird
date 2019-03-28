96111ad2ee8ff209e34ced93c8c54d8dfefce8b1
Denis Gordin <gordin.dan@gmail.com>
2019-03-28 15:34:10 -0400

    Add simple animation to the bird

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -1,28 +1,60 @@
 import 'dart:ui';
+import 'package:flame/animation.dart';
+import 'package:flame/components/animation_component.dart';
 import 'package:flame/game.dart';
-import 'package:flutter_bird/game/bird.dart';
+import 'package:flame/sprite.dart';
+import 'package:flutter_bird/game/config.dart';
 import 'package:flutter_bird/game/horizont.dart';
 
 enum FlutterBirdGameStatus { playing, waiting, gameOver }
 
 class FlutterBirdGame extends BaseGame {
   Horizon horizon;
-  Bird bird;
+  AnimationComponent anim;
+  // Bird bird;
   FlutterBirdGameStatus status = FlutterBirdGameStatus.waiting;
 
   double timePlaying = 0.0;
 
   FlutterBirdGame({Image spriteImage}) {
-    horizon = new Horizon(spriteImage);
-    bird = new Bird(spriteImage);
-    this..add(horizon)..add(bird);
 
+    horizon = new Horizon(spriteImage);
+
+    List<Sprite> sprites = [
+      Sprite.fromImage(
+        spriteImage,
+        width: Dimensions.birdWidth,
+        height: Dimensions.birdHeight,
+        y: 491.0,
+        x: 3.0,
+      ),
+      Sprite.fromImage(
+        spriteImage,
+        width: Dimensions.birdWidth,
+        height: Dimensions.birdHeight,
+        y: 491.0,
+        x: 31.0,
+      ),
+      Sprite.fromImage(
+        spriteImage,
+        width: Dimensions.birdWidth,
+        height: Dimensions.birdHeight,
+        y: 491.0,
+        x: 59.0,
+      )
+    ];
+
+    var animatedBird = new Animation.spriteList(sprites, stepTime: 0.15);
+    anim = AnimationComponent((51), 36, animatedBird);
+    anim.x = 150;
+    anim.y = 150;
+    
+    this..add(horizon)..add(anim);
   }
 
   @override
   void update(double t) {
-    // timePlaying += t;
-    // horizon.updateWithSpeed(0.0, this.currentSpeed);
+    anim.update(t);
   }
 
   bool get playing => status == FlutterBirdGameStatus.playing;
